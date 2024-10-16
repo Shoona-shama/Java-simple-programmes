179b425839df05b664e8a4e795617890d062bc91
Shoona-shama <rishihab@protonmail.com>
2024-10-16 16:11:57 +0200

    second commit

--- src/LongestWordInString.java
+++ src/LongestWordInString.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
-public class Main {
+public class LongestWordInString {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
