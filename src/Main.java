efa97f34ded56d9eb745c1dec47df6313ad2013d
Shoona-shama <rishihab@protonmail.com>
2024-10-16 16:07:35 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String strVomNutzer = sc.nextLine();
+        String regex = " ";
+        String[] arrVomStr = strVomNutzer.split(regex);
+        System.out.println(Arrays.toString(arrVomStr));
+
+        String laengstesWort = "";
+        for (String wort : arrVomStr) {
+            if (wort.length() > laengstesWort.length()) {
+                laengstesWort = wort;
+            }
+        }
+        System.out.println("Das längste Wort ist: " + laengstesWort);
+    }
+}
